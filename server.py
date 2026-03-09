85e0fd808277a44c26af01bd07390243e5f77939
Josephine Mui <josephinemui@yahoo.com>
2026-03-09 15:06:20 -0500

    Add files

--- server.py
+++ server.py
@@ -16,16 +16,60 @@ def index():
 
 @app.route('/speech-to-text', methods=['POST'])
 def speech_to_text_route():
-    return None
+    print("processing speech-to-text")
+
+    audio_binary = request.data # Get the user's speech from request
+    text = speech_to_text(audio_binary)
+
+    # Return the response back to the user in JSON format
+    # Use the Flask's app.response_class function to create the response
+    # response=json.dumps is the actual data sent in the body of our HTTP response
+    response = app.response_class(
+        response=json.dumps({'text': text}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    print(response)
+    print(response.data)
+    return response
 
 
 @app.route('/process-message', methods=['POST'])
 def process_prompt_route():
+    user_message = request.json['userMessage']
+    print('user_message', user_message)
+
+    voice = request.json['voice']
+    print('voice', voice)
+
+    response_text = openai_process_message(user_message)
+
+    # Clean the response to remove any empty lines
+    response_text = os.linesep.join([s for s in response_text.splitlines() if s])
+
+    # Call text_to_speech to convert OpenAI Api's response to speech
+    response_speech = text_to_speech(response_text, voice)
+
+    # Convert sppech to base64 string so it can be sent back in JSON response
+    '''
+    As the openai_response_speech is a type of audio data, we can't directly 
+    send this inside a json as it can only store textual data. Therefore, we 
+    will be using something called "base64 encoding". In simple words, we can 
+    convert any type of binary data to a textual representation by encoding 
+    the data in base64 format. 
+    '''
+    response_speech = base64.b64encode(response_speech).decode('utf-8')
+
+	# Send a JSON response back to the user containing their message's response both in text 
+    # and speech formats
     response = app.response_class(
-        response=json.dumps({"openaiResponseText": None, "openaiResponseSpeech": None}),
+        response=json.dumps({"openaiResponseText": response_text, "openaiResponseSpeech": response_speech}),
         status=200,
         mimetype='application/json'
     )
+
+    print(response)
     return response
 
 
--- worker.py
+++ worker.py
@@ -5,12 +5,131 @@ openai_client = OpenAI()
 
 
 def speech_to_text(audio_binary):
-    return None
+
+    # Set up Watson Speech-to-Text HTTP Api url
+    base_url = "https://sn-watson-stt.labs.skills.network"
+    api_url = base_url + '/speech-to-text/api/v1/recognize'
+
+    # Set up parameters for our HTTP request
+    # Tells Watson to use the US English model for processing the speech
+    params = {
+        'model': 'en-US_Multimedia',
+    }
+
+    # Send a HTTP Post request
+    response = requests.post(api_url, params=params, data=audio_binary).json()
+
+    '''
+    Structure of the response:
+
+    {
+        "response": {
+            "results": {
+                "alternatives": {
+                    "transcript": "Recognised text from your speech"
+                }
+            }
+        }
+    }   
+    '''
+
+    # Parse the response to get the transcribed text
+    text = 'null'
+    while bool(response.get('results')):
+        print('speech to text response:', response)
+        text = response.get('results').pop().get('alternatives').pop().get('transcript')
+        print('recognized text: ', text)
+        return text
 
 
 def text_to_speech(text, voice=""):
-    return None
+    base_url = "https://sn-watson-tts.labs.skills.network"
+    api_url = base_url + "/text-to-speech/api/v1/synthesize?output=output_text.wav"
+
+    # Adding voice parameter in api_url if the user has selected a preferred voice
+    if voice != "" and voice != "default":
+        api_url += "&voice=" + voice
+
+    # Set the headers for HTTP request
+    # "Accept": "audio/wav" => sending an audio wav format
+    # "Content-Type": application/json" => format of the body is JSON
+    headers = {
+        "Accept": "audio/wav",
+        "Content-Type": "application/json",
+    }
+
+    # Set the body of HTTP request
+    json_data = { "text": text }
+
+    # Send HTTP Post request
+    response = requests.post(api_url, headers=headers, json=json_data)
+
+    '''
+    Structure of the response:
+
+    {
+        "response": {
+                content: The Audio data for the processed text to speech
+            }
+        }
+    }
+    '''
+    print("text to speech response:", response)
+
+    return response.content
 
 
 def openai_process_message(user_message):
-    return None
+
+    # Set the prompt for OpenAI Api
+    # Tell the model to become a personal assistant.
+    # Giving it specific tasks
+    # By adding the original user message afterwards, it gives OpenAI more
+    # room to sound genuine.
+    prompt = "Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations. Keep responses concise - 2 to 3 sentences maximum."
+
+    client = OpenAI()
+
+    # Call the OpenAI Api to process our prompt
+    '''
+    The messages parameter is an array of objects used to define the conversation
+    flow between the user and the AI. Each object represents a message with two key
+    attributes: role (identifying the sender as either "system" for setup instructions
+    or "user" for the actual user query) and content (the message text).
+    
+    The "system" role message instructs the AI on how to behave (for example, acting
+    like a personal assistant), while the "user" role message contains the user's input.
+    This structured approach helps tailor the AI's responses to be more relevant and
+    personalized.
+    '''
+    response = client.chat.completions.create(
+        model="gpt-5-nano",
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": user_message}
+        ],
+        max_completion_tokens=1000
+    )
+
+    '''
+    Structure of the response:
+
+    {
+        "choices": [
+                {"message": {
+                    content: "The model\'s answer to our prompt",
+                    ...,
+                    ...,
+                },
+                ...,
+                ...
+            ]
+    }
+   
+    '''
+    print("openai response:", response)
+
+    # Parse the response to get the message
+    response_text = response.choices[0].message.content
+
+    return response_text
